60212dee7821e181d9950dbffa2979368aea65e2
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-06-01 22:55:30 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ $result = $db->query($selectQuery);
 <!DOCTYPE html>
 <html>
   <head>
+    <title>Name Saver</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
